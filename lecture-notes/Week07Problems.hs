807c298fd90306b53c2b28f3797e1dec61c91a5b
Steven Muirhead CESM2019 <steven.muirhead.2017@uni.strath.ac.uk>
2021-11-27 19:22:55 +0000

    finished week 7

--- lecture-notes/Week07Problems.hs
+++ lecture-notes/Week07Problems.hs
@@ -46,13 +46,22 @@ instance Monad Result where
             Error e -> Error e 
             Ok a    -> f a
 
-search:: (Eq k, Show k) => k -> [(k, v)] -> Result v
+search :: (Eq k, Show k) => k -> [(k, v)] -> Result v
 search k [] =  Error ("Key "++ show k ++" not present")
 search k ((k',v'):kvs) =
    if k == k' then
-     return v'
+      return v'
     else
       search k kvs 
+
+lookupAll :: (Eq k, Show k) => [(k,v)] -> Tree k -> Result (Tree v)
+lookupAll kvs Leaf = Ok Leaf
+lookupAll kvs (Node l v r) = 
+  do 
+    l' <- lookupAll kvs l
+    v' <- search v kvs
+    r' <- lookupAll kvs r
+    return (Node l' v' r')
 
 {- 2. Write a function using the Printing monad and 'do' notation that
       "prints out" all the strings in a tree of 'String's: -}
@@ -79,18 +88,87 @@ newtype StateDouble a = MkStateDouble (Double -> (Double, a))
 runStateDouble :: StateDouble a -> Double -> (Double, a)
 runStateDouble (MkStateDouble t) = t
 
+instance Monad StateDouble where 
+  return :: a -> StateDouble a 
+  return x = MkStateDouble (\s -> (s, x))
+
+  (>>=) :: StateDouble a -> (a -> StateDouble b) -> StateDouble b
+  op >>= f =
+     MkStateDouble (\s -> 
+       let (s0, a) = runStateDouble op s
+           (s1, b) = runStateDouble (f a) s0
+           in (s1, b))
+
+getDouble :: StateDouble Double
+getDouble = MkStateDouble (\s -> (s,s))
+
+putDouble :: Double -> StateDouble ()
+putDouble i = MkStateDouble (\_ -> (i,()))
+
+sumImpDouble :: [Double] -> StateDouble Double 
+sumImpDouble xs = 
+  do 
+    putDouble 0
+    for_ xs (\x -> 
+      do 
+        total <- getDouble
+        putDouble (total + x))
+    getDouble
+        
 {-    (b) Make an alternative version of 'State' that is parameterised
           by the type of the state (so that someone using it can
           decide whether it is 'Int' or 'Double' for instance). -}
 
 
+newtype StateGeneric s a = MkStateGeneric (s -> (s,a))
+
+runStateGeneric :: StateGeneric s a -> s -> (s,a)
+runStateGeneric (MkStateGeneric a) = a
+
+instance Monad (StateGeneric s) where
+  return :: a -> StateGeneric s a
+  return a = MkStateGeneric (\s -> (s,a))
+
+  (>>=) :: StateGeneric s a -> (a -> StateGeneric s b) -> StateGeneric s b
+  op >>= f = 
+    MkStateGeneric (\s ->
+      let (s0, a) = runStateGeneric op s
+          (s1, b) = runStateGeneric (f a) s0
+      in (s1, b))
+
+getGeneric :: StateGeneric s s
+getGeneric = MkStateGeneric (\s -> (s,s)) 
+
+putGeneric :: s -> StateGeneric s () 
+putGeneric s = MkStateGeneric (\_ -> (s,()))
+
+sumImpGeneric :: Monoid m => [m] -> StateGeneric m m
+sumImpGeneric xs = 
+  do 
+    putGeneric mempty
+    for_ xs (\x -> 
+      do 
+      total <- getGeneric
+      putGeneric (total <> x))
+    getGeneric
+
 {- 4. Write a function like mapM that works on 'Tree's instead of lists: -}
 
 mapTreeM :: Monad m => (a -> m b) -> Tree a -> m (Tree b)
-mapTreeM = undefined
-
+mapTreeM f Leaf = return Leaf
+mapTreeM f (Node l v r) =
+  do 
+    l' <- mapTreeM f l
+    v' <- f v
+    r' <- mapTreeM f r
+    return (Node l' v' r')
 
 {- 5. Write a function like mapM that works on 'Maybe's instead of lists: -}
 
 mapMaybeM :: Monad m => (a -> m b) -> Maybe a -> m (Maybe b)
-mapMaybeM = undefined
+mapMaybeM f Nothing = return Nothing 
+mapMaybeM f (Just x) = 
+  -- f x >>= \y -> return (Just y)    the "raw way"
+  do
+    y <- f x
+    return (Just y)
